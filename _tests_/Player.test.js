3748d8cc3f435843f69e9178fcd4b03919c0f1e4
Lorreina Guyett <slorreina369@gmail.com>
2022-07-05 22:31:52 -0700

    health functions added

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,5 +1,6 @@
 const Player = require('../lib/Player');
 
+
 test('creates a player object', () =>{
     const player = new Player('Dave');
 
@@ -28,9 +29,32 @@ test('gets inventory from player or returns false', () =>{
 
     expect(player.getInventory()).toEqual(false);
 });
+test("get player's health value", () =>{
+    const player = new Player('Dave');
 
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+test('checks if player is alive or not', () =>{
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+test("subtracts from player's health", () =>{
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
-console.log(new Potion());
